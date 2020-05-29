032bcea3a03c90c4af962c3f5f55e71445320b90
Sara <saragmzgrc@gmail.com>
2020-05-29 13:41:46 +0200

    getOrders filtered by user

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,12 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 
-const firestone = admin.firestore();
+const firestore = admin.firestore();
 
 exports.getAllProducts =
     functions.https.onRequest(
         async (req, res) => {
-            const snapshot = await firestone
+            const snapshot = await firestore
                 .collection('products')
                 .get();
             const parsed = snapshot.docs.map(doc => doc.data());
@@ -17,12 +17,33 @@ exports.getAllProducts =
     );
 
 exports.getAllCategories = 
-        functions.https.onRequest(
-            async (req, res) => {
-                const snapshot = await firestone
-                    .collection('categories')
-                    .get();
-                const parsed = snapshot.docs.map(doc => doc.data());
-                res.send({ data: parsed });
-            }
-        )
+    functions.https.onRequest(
+        async (req, res) => {
+            const snapshot = await firestore
+                .collection('categories')
+                .get();
+            const parsed = snapshot.docs.map(doc => doc.data());
+            res.send({ data: parsed });
+        }
+    );
+
+exports.getProductsByUserId = functions.https.onCall(async (data, context) => {
+    const snapshot = await firestore
+            .collection('orders')
+            .get();
+    const parsed = snapshot.docs.map(doc => doc.data());
+    
+    return ({ data: parsed  });
+    
+});
+
+// exports.getProductsByUserId =
+//     functions.https.onRequest(
+//         async (req, res) => {
+//             // const collectionRef = firestore.collection('orders')
+
+//             // const snapshot = await collectionRef.where('userId' === data.userId);
+//             // const parsed = snapshot.docs.map(doc => doc.data());
+//             res.send({ data: req  });
+//         }
+//     );
